a785d18f87cd025c359b86734864ab76e9b271b4
Evan <evangeorgemarshall@gmail.com>
2022-04-20 15:58:04 -0700

    Upgrade npm release package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { UserSession, MockOracleSession } = require("./app/sessions.js");
 
 const SOLRAND_IDL = require("./app/solrand.json");
-const PROGRAM_ID = "GxJJd3q28eUd7kpPCbNXGeixqHmBYJ2owqUYqse3ZrGS";
+const PROGRAM_ID = "8nzxsNf74ZHDguHi51SjQWxDxegL2DBgxeGHA2pQVtTJ";
 const ORACLE_DEVNET = "qkyoiJyAtt7dzaUTsiQYYyGRrnJL3AE1mP93bmFXpY8";
 
 module.exports = { UserSession, MockOracleSession, SOLRAND_IDL, PROGRAM_ID, ORACLE_DEVNET }
